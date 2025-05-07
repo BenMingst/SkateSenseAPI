8fb6b497801ec1e66d766eafcddad93b753f5a64
Benjamin Mingst <benjaminmingst@outlook.com>
2025-05-07 10:45:07 -0400

    commit two

--- app/src/main/java/com/example/skatesenseapi/data/DisplayData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.skatesenseapi.data
-
-data class DisplayData(
-    val temperature: String,
-    val humidity: String,
-    val wind: String,
-    val speed: String,
-    val stopwatch: String
-)
